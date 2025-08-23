d56f36b15974e9e340f6aec3446b0c76bb775619
nicolas <pavonnicolas11@gmail.com>
2025-08-23 16:53:47 -0300

    Agregando cors

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ import { userRouter } from './routes/user.js';
 import { boardRouter } from './routes/board.js';
 import { taskRouter } from './routes/task.js';
 import { authRouter } from './routes/auth.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}));
 app.disable('x-powered-by');
 
 
