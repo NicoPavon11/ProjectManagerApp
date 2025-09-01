42e58301126e7a3f87c9506365e87828101e9ac3
nicolas <pavonnicolas11@gmail.com>
2025-09-01 19:21:06 -0300

    cambio cookie

--- controllers/auths.js
+++ controllers/auths.js
@@ -33,7 +33,7 @@ export class AuthController {
                     .cookie('access_token', token, {
                         httpOnly: true,
                         secure: process.env.NODE_ENV === 'production',
-                        sameSite: 'strict',
+                        sameSite: 'lax',
                         maxAge: 1000 * 60 * 60
                     })
                     .status(200)
